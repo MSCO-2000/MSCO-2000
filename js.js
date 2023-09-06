fd86062ffe21c3288b701851104ce9beb55e5129
Martín Calderón <elmcwl@gmail.com>
2023-09-06 09:09:35 -0700

    Delete js.js

--- js.js
+++
@@ -1,8 +0,0 @@
-$.ajax({
-  type: 'GET',
-  url: 'https://wakatime.com/share/@dsw47/748daae6-5517-45ba-b3a6-9d357d11fd6c.json',
-  dataType: 'jsonp',
-  success: function(response) {
-    console.log(response.data);
-  },
-});
